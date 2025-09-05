629498ad93e91a86cbde856ccb67622dd23be222
Jeremy-Ortega <jeremysortega@gmail.com>
2025-09-05 23:55:18 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,18 @@
+const name = "Jeremy";
+const age = 20 ;
+const isStudent = true;
+//-------------------------------------------- 
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isStudent));
+//-------------------------------------------- 
+
+console.log(10+10);
+console.log(10-10);
+console.log(10*10);
+console.log(10/10);
+//-------------------------------------------- 
+
+console.log("5" == 5);
+console.log("5"=== 5);
