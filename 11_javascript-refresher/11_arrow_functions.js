48c28b7e31349609ae528be83a19f1873cc10024
Jeremy-Ortega <jeremysortega@gmail.com>
2025-09-06 00:00:49 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,18 @@
+
+const greet = (name) => {
+ return "Hello, " + name; };
+console.log(greet("Jeremy"));
+
+//--------------------------------------------
+
+const square = n => n*n;
+
+console.log(square(5));
+
+//--------------------------------------------
+
+const sayHi = () => {
+ console.log("Hi!"); };
+sayHi();
+
+//--------------------------------------------
