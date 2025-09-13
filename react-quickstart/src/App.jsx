7a9e5a738b3fe567cfeaedfbc94515779252d921
Jeremy-Ortega <jeremysortega@gmail.com>
2025-09-13 10:27:44 +0800

    Done creating and nesting components

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+
+// Creating and nesting components
+// Writing markup with JSX
+// Adding styles
+// Displaying data
+// Conditional rendering
+
+function App() {
+
+  function MyButton(){
+    return(
+    <button>
+      I'm a button :D
+    </button>
+    );
+  }
+
+  return (
+    <>
+    <div>
+      <h1>Hi, welcome to my App</h1>
+      <MyButton/>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
