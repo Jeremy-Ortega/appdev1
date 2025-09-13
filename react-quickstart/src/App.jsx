36d62b0299327d5c7635e1701539ef0e017dc0fe
Jeremy-Ortega <jeremysortega@gmail.com>
2025-09-13 10:49:02 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -8,6 +8,12 @@ import './App.css'
 // Conditional rendering 
 
 function App() {
+  const User = {
+    name : "Jeremy",
+    age : 20,
+    imgUrl : "assets\sample.png",
+    imgSize : 90
+  }
 
   function MyButton(){
     return(
@@ -20,9 +26,16 @@ function App() {
   return (
     <>
     <div>
-      <h1>Hi, welcome to my App</h1>
-      <h4 style={{color:'lightcoral'}}>Hello<br />How do you do?</h4>
+      <h1>Welcome to my App</h1>
+      <h4 style={{color:'lightcoral'}}>Hello I'm {User.name} <br />How do you do?</h4>
       <MyButton/>
+      <br />
+
+      <img src={User.imgUrl} alt="Photo of the pokemon named Vaporeon"  
+      style={{
+        width:User.imgSize,
+        height:User.imgSize
+      }}/>      
     </div>
 
 
