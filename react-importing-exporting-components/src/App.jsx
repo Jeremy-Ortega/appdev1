44c07e87d5ebccb40e391e8d0b382a2b0264d945
Jeremy-Ortega <jeremysortega@gmail.com>
2025-11-09 18:59:12 +0800

    Done with exporting and importing multiple components from the same file

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,9 +1,13 @@
 import Gallery from "./components/Gallery";
+import {Profile} from "./components/Gallery";
 
 export default function App() {
   return (
     <>
+    <h1 style={{ fontStyle: 'italic'}}> Imported component : </h1>
     <Gallery />
+    <h1 style={{ fontStyle: 'italic'}}> Named export from Gallery : </h1>
+    <Profile/>
     </>
   );
 }
