acb75fb6f11f83201f2c28c7b6640e2d82d4ab88
Jeremy-Ortega <jeremysortega@gmail.com>
2025-11-09 19:05:35 +0800

    Done with Converting HTML to JSX

---
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* // Not working if copy pasted */}
+    <h1>Hedy Lamarr's Todos</h1>
+    <img 
+      src="https://i.imgur.com/yXOvdOSs.jpg" 
+      alt="Hedy Lamarr" 
+      class="photo"
+    >
+    <ul>
+        <li>Invent new traffic lights
+        <li>Rehearse a movie scene
+        <li>Improve the spectrum technology
+    </ul>
+
+
+    </>
+  )
+}
+
+export default App
